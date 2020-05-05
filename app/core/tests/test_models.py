fe1b3c4c33e8e5ddb46cf58a9e28b4a57051382c
lunesco <neuromind@vp.pl>
2020-05-05 17:42:21 +0200

    all files have been replaced with files from an external project for the docker to work

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,13 +1,13 @@
-from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        email = 'test@gmail.com'
-        password = 'Testpass123'
+        email = 'test@londonappdev.com'
+        password = "Testpass123"
         user = get_user_model().objects.create_user(
             email=email,
             password=password
